d9049e8eebf03596c6b141ac313eaec97365699b
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2022-11-29 21:39:39 +0300

    + Unique

--- Tests/Main.cpp
+++ Tests/Main.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 void Rand(int arr[], const int n, int min = 0, int max = 9);
 void Print(int arr[], const int n);
+int minValueIn(int arr[], const int n);
+int maxValueIn(int arr[], const int n);
 void ShiftLeft(int arr[], const int n, int numOfShifts);
 void ShiftRight(int arr[], const int n, int numOfShifts);
-void Unique(int arr[], const int n);
+int Unique(int arr[], const int n);
 
 void main ()
 {
@@ -14,11 +16,14 @@ void main ()
 	int numOfShifts;
 	Rand(arr, n);
 	Print(arr, n);
+	cout << "Min value: " << minValueIn(arr, n) << endl;
+	cout << "Max value: " << maxValueIn(arr, n) << endl;
 	cout << "Enter the number of shifts of the array: "; cin >> numOfShifts;
 	ShiftLeft(arr, n, numOfShifts);
 	cout << endl;
 	ShiftRight(arr, n, numOfShifts);
-	Unique(arr, n);
+	Unique(arr, n); Print(arr, n);
+	
 }
 
 void Rand(int arr[], const int n, int min, int max) 
@@ -36,6 +41,24 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 }
+int minValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+int maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
 void ShiftLeft(int arr[], const int n, int numOfShifts)
 {
 	for (int i = 0; i < numOfShifts; i++)
@@ -46,11 +69,7 @@ void ShiftLeft(int arr[], const int n, int numOfShifts)
 			arr[i] = arr[i + 1];
 		}
 		arr[n - 1] = buff;
-		cout << endl;
-		for (int i = 0; i < n; i++)
-		{
-			cout << arr[i] << "\t";
-		}
+		Print(arr, n);
 	}
 	cout << endl;
 }
@@ -64,26 +83,14 @@ void ShiftRight(int arr[], const int n, int numOfShifts)
 			arr[i + 1] = arr[i];
 		}
 		arr[0] = buff;
-		for (int i = 0; i < n; i++)
-		{
-			cout << arr[i] << "\t";
-		}
-		cout << endl;
+		Print(arr, n);
 	}
 	cout << endl;
 }
-void Unique(int arr[], const int n)
+int Unique(int arr[], const int n)
 {
-	int min = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] < min)min = arr[i];
-	}
-	int max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
-	}
+	int min = minValueIn(arr, n);
+	int max = maxValueIn(arr, n) + 2;
 	bool random;
 	for (int i = 0; i < n;)
 	{
@@ -98,13 +105,9 @@ void Unique(int arr[], const int n)
 			}
 		}
 		if (random == false)
-		{
+		{ 
 			arr[i] = ranVal; i++;
 		}
-		for (int i = 0; i < n; i++)
-		{
-			cout << arr[i] << "\t";
-		}
 	}
-	
+	return arr[n];
 }
