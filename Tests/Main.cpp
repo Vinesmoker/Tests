4df619e79228c4ccaa04603b5873cbb54b621ca6
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2022-12-21 00:09:54 +0300

    Test with recursion

--- Tests/Main.cpp
+++ Tests/Main.cpp
@@ -1,29 +1,48 @@
 #include<iostream>
 using namespace std;
 
-void Unique(float arr[], const int n, int min = 0, int max = 9);
+int Factorial(int f);
+double Pow(double num, int degree);
+void FibLimit(int lim, int a = 1, int b = 1);
+void FibQuantity(int quant, int count = 0, long a = 1, long b = 1);
 
 void main ()
 {
-	const int n = 10;
-	float arr[n]{};
-	Unique(arr, n); cout << endl;
+	setlocale(LC_ALL, "");
+	int f; double num; int degree; int lim;
+	int quant;
+	cout << "Введите число для вычисления его факториала: "; cin >> f;
+	cout << "Результат: " << Factorial(f) << endl;
+	cout << "Введите число для возведения в степень: "; cin >> num;
+	cout << "Введите степень для возведения в нее числа " << num << ": "; cin >> degree;
+	cout << "Результат: " << Pow(num, degree) << endl;
+	cout << "Введите предельное число ряда Фибоначи: "; cin >> lim;
+	cout << "Результат: "; FibLimit(lim); cout << endl;
+	cout << "Введите необходимое количество элементов ряда Фибоначи: "; cin >> quant;
+	cout << "Результат: " << endl;
+	FibQuantity(quant);
 }
 
-void Unique (float arr[], const int n, int min, int max)
+int Factorial(int f)
 {
-	int c = n;
-	for (int i = 0; i < c; i++)
-	{
-		while ((max - min) < n)max++;
-		bool ran = false;
-		arr[i] =  min + rand() % (max - min); arr[i] /= 100;
-		float buff = arr[i];
-		for (int j = 0; j < i; j++)
-		{
-			if (i == j)continue;
-			if (buff == arr[j] && i != j)ran = true; c++;
-		}
-		if (!ran)cout << buff << "\t"; 
-	}
+	if (f == 0)return 1;
+	else return f * Factorial(f - 1);
+}
+double Pow(double num, int degree)
+{
+	if (degree == 0)return 1;
+	if (degree < 0)return 1 / num * Pow(num, degree + 1);
+	else return num * Pow(num, degree - 1);
+}
+void FibLimit(int lim, int a, int b)
+{
+	if (a > lim)return;
+	cout << a << "\t";
+	FibLimit(lim, b, a + b);
+}
+void FibQuantity(int quant, int count, long a, long b)
+{
+	if (count > quant)return;
+	cout << count << "\t" << a << "\n";
+	FibQuantity(quant, count + 1, b, a + b);
 }
